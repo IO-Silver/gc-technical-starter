4dffd5b5e4d4b4ddb293ecd38d7b2aa1d6e232c6
Rio Silver <rjcsilver@gmail.com>
2024-08-21 16:40:26 -0400

    Whoops, undid my index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { React } from 'react';
+import Logo from '../components/Logo';
+import BillingProblem from '../components/BillingProblem';
 
 export default function Home() {
   return (
@@ -7,7 +10,12 @@ export default function Home() {
         <title>Track-Stack</title>
       </Head>
 
-      <div>Let's do it</div>
+      <Logo />
+      <BillingProblem errorMessage="It looks like your billing method on file may not be setup correctly."
+                      reason="You have $1,204.03 balance owing for longer than 20 days."
+                      primaryAction="View All Invoices"
+                      secondaryAction="Fix Payment Method & Pay Now"
+                      reconciliation="Once the payment method is fixed and balance is paid, you'll be able to access your account again." />
     </div>
   )
 }
